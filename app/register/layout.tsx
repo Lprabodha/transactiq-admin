195c19c532336004945fcb246647856624a16273
Lprabodha <lprabodha1998@gmail.com>
2025-06-15 17:43:24 +0530

    Dashboard design improve

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -1,8 +1,6 @@
-import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { DashboardLayout } from "@/components/dashboard-layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,14 +12,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <DashboardLayout>{children}</DashboardLayout>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
